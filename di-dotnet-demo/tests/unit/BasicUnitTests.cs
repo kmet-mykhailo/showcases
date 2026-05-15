97bb1f19e8a1fcb49c76c59aa07e0184699dd3cd
Mykhailo Kmet(macbook) <kmetmisha@gmail.com>
2026-05-15 16:19:15 +0300

    add di-dotnet-demo solution

---
+++ di-dotnet-demo/tests/unit/BasicUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DI.UnitTests;
+
+public class BasicUnitTests
+{
+    [Fact]
+    public void AddSingletone_ReturnsSameInstance()
+    {
+        // Arrange
+        IServiceCollection services = new ServiceCollection();
+        services.AddSingleton<IServiceA, ServiceA>();
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        // Act
+        IServiceA instance1 = serviceProvider.GetRequiredService<IServiceA>();
+        IServiceA instance2 = serviceProvider.GetRequiredService<IServiceA>();
+
+        // Assert
+        Assert.Equal(instance1, instance2);
+    }
+
+    [Fact]
+    public void Test()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    private interface IServiceA { }
+    private sealed class ServiceA : IServiceA { }
+}
