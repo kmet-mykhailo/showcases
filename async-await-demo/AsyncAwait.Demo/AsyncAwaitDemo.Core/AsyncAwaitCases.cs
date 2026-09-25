ab98091e1cc52e131b552d9c9a16676ab5dda385
Mykhailo Kmet(macbook) <kmetmisha@gmail.com>
2026-09-25 23:00:47 +0300

    async await demo updated

---
+++ async-await-demo/AsyncAwait.Demo/AsyncAwaitDemo.Core/AsyncAwaitCases.cs
@@ -0,0 +1,10 @@
+﻿namespace AsyncAwaitDemo.Core;
+
+public static class AsyncAwaitCases
+{
+    public static async Task DoSomethingWithExceptionAsync(CancellationToken cancellationToken = default)
+    {
+        await Task.Delay(100, cancellationToken);
+        throw new Exception();
+    }
+}
---
+++ async-await-demo/AsyncAwait.Demo/tests/AsyncAwaitDemo.UnitTests/AsyncAwaitUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using AsyncAwaitDemo.Core;
+
+namespace UnitTests;
+
+public class AsyncAwaitUnitTests
+{
+    [Fact]
+    public void DoSomethingWithExceptionAsync_WhenNotAwaited_DoesNotThrow()
+    {
+        // Act
+        AsyncAwaitCases.DoSomethingWithExceptionAsync();
+        
+        // Assert
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public async Task DoSomethingWithExceptionAsync_WhenAwaited_Throw()
+    {
+        // Act && Assert
+        var exception =  await Assert.ThrowsAsync<Exception>(() => AsyncAwaitCases.DoSomethingWithExceptionAsync());
+        Assert.NotNull(exception);
+    }
+    
+    [Fact]
+    public async Task DoSomethingWithExceptionAsync_WhenNotAwaitedTryCatch_MissExceptionThrown()
+    {
+        // Arrange
+        Task NotAwaitedTryCatch()
+        {
+            try
+            {
+                return AsyncAwaitCases.DoSomethingWithExceptionAsync();
+            }
+            catch(Exception ex)
+            {
+                // never catch exception
+                return Task.FromResult(ex.Message);
+            }
+        }
+        
+        // Act & Assert
+        await Assert.ThrowsAsync<Exception>(NotAwaitedTryCatch);
+    }
+    
+    [Fact]
+    public async Task DoSomethingWithExceptionAsync_WhenNotAwaitedUsing_MissExceptionThrown()
+    {
+        // Arrange
+        Task NotAwaitedUsing()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource();
+            return AsyncAwaitCases.DoSomethingWithExceptionAsync(cancellationTokenSource.Token);
+        }
+        
+        // Act & Assert
+        await Assert.ThrowsAsync<Exception>(NotAwaitedUsing);
+    }
+}
